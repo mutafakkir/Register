725dcaaafb0e6380cf94e57fa74525b99abed9c0
ASR0RB3K <asrorbek.design@gmail.com>
2022-01-20 09:52:54 +0500

    Sing Up

---
+++ ViewModels/SignUpViewModel.cs
@@ -0,0 +1,49 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace register.ViewModels;
+
+public class SingUpViewModel : IValidatableObject
+{
+    [Required(ErrorMessage = "Введите Имя и Фамилию")]
+    [Display(Name = "Имя Фамилия")]
+    public string Fullname { get; set; }
+
+    [Required(ErrorMessage = "Введите дату рождения")]
+    [Display(Name = "Дата рождения")]
+    public DateTimeOffset Birthdate { get; set; }
+    
+    [Required(ErrorMessage = "Введите номер телефона")]
+    [Phone(ErrorMessage = "Формат телефон номера неправельный.")]
+    [Display(Name = "Телефон номер")]
+    public string Phone { get; set; }
+    
+    [Required(ErrorMessage = "Введите ваш email адрес")]
+    [EmailAddress(ErrorMessage = "Неправельный формат email адреса.")]
+    [Display(Name = "Ваш email")]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "Введите пароль")]
+    [MinLength(6, ErrorMessage = "Пароль должен состоять не менее из 6 символов.")]
+    [Display(Name = "Пароль")]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Подтвердите пароль")]
+    [MinLength(6, ErrorMessage = "Пароли не совпадают, проверьте еще раз.")]
+    [Display(Name = "Подтвердить пароль")]
+    public string ConfirmPassword { get; set; }
+    
+    [Display(Name = "Запомнить")]
+    public bool RememberMe { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        var now = DateTimeOffset.Now;
+        var limit = new DateTime(now.Year - 13, now.Month, now.Day);
+        Console.WriteLine($"{limit} {Birthdate}");
+
+        if(Birthdate > limit)
+        {
+            yield return new ValidationResult($"Вы должны быть старше 13 лет!", new [] { nameof(Birthdate)});
+        }
+    }
+}
