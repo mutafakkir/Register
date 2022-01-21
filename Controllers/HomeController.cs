b196d5018aa803f824f2cfd07e6bd1869bb9edd1
ASR0RB3K <asrorbek.design@gmail.com>
2022-01-21 16:27:56 +0500

    Db Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using register.Entity;
 using register.Models;
 
 namespace register.Controllers;
@@ -7,10 +10,18 @@ namespace register.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<User> _useM;
+    private readonly SignInManager<User> _signInManager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(
+        ILogger<HomeController> logger,
+        UserManager<User> useManager,
+        SignInManager<User> signInManager
+    )
     {
         _logger = logger;
+        _useM = useManager;
+        _signInManager = signInManager;
     }
 
     public IActionResult Index()
@@ -21,6 +32,13 @@ public class HomeController : Controller
     public IActionResult Privacy()
     {
         return View();
+    }
+
+    [Authorize]
+    public IActionResult Secret()
+    {
+        var users = _useM.Users.ToList();
+        return View(users);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
